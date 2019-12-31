50fc4dc345ab3db7421c87ed52118631771d2f34
xiaoxujs <18749875170@163.com>
2019-12-31 13:56:17 +0800

    防抖

--- encapsulationFun/xiaoxujsFun.js
+++ encapsulationFun/xiaoxujsFun.js
@@ -6,3 +6,12 @@ export const isEmpty = ( obj ) => {
         return false;
     }
 }
+
+//防抖
+var timeout = null;
+export const antiShake = (fn, ms = 500) => {
+    clearTimeout(timeout);  // 每次触发时先清除上一次的定时器,然后重新计时
+    timeout = setTimeout(()=>{
+        fn()
+    }, ms); 
+}
